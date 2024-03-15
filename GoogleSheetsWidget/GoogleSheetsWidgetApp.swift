a8dd40b9acf084963d0f362d6bf8c8acc24e45d3
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2024-03-15 13:17:52 +0200

    Initial Commit

---
+++ GoogleSheetsWidget/GoogleSheetsWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoogleSheetsWidgetApp.swift
+//  GoogleSheetsWidget
+//
+//  Created by Alexandr Marchenko on 15.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct GoogleSheetsWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
