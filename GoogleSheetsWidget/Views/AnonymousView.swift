708a97da671bc008c857f63ed19135235daa2ed7
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2024-03-15 15:50:03 +0200

    playgrounds

---
+++ GoogleSheetsWidget/Views/AnonymousView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct AnonymousView: View {
+    var body: some View {
+        Text("Anonymous")
+    }
+}
+
+#Preview {
+    AnonymousView()
+}
---
+++ GoogleSheetsWidget/Views/AuthenticatedView.swift
@@ -0,0 +1,11 @@
+import SwiftUI
+
+struct AuthenticatedView: View {
+    var body: some View {
+        Text("Authenticated")
+    }
+}
+
+#Preview {
+    AuthenticatedView()
+}
