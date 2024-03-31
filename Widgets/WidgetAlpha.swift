3ecdb3d813752b9c0e5fdf2fde6671e0d873187c
Alexandr Marchenko <marchenko.alexandr@gmail.com>
2024-03-31 18:46:19 +0300

    widget intent

--- Widgets/WidgetAlpha.swift
+++ Widgets/WidgetAlpha.swift
@@ -1,7 +1,9 @@
+import AppIntents
 import WidgetKit
 import SwiftData
 import SwiftUI
 import Shared
+
 
 struct WidgetAlphaEntry: TimelineEntry {
     let date: Date
@@ -12,7 +14,7 @@ struct WidgetAlphaView : View {
     var entry: WidgetAlphaEntry
     var body: some View {
         VStack(alignment: .leading) {
-            Text("item 0").font(.caption).foregroundStyle(.secondary)
+            Text(self.entry.value.isEmpty ? "item 0": self.entry.value).font(.caption).foregroundStyle(.secondary)
             HStack {
                 Text("2.4%").font(.title)
                 Spacer()
@@ -34,66 +36,99 @@ struct WidgetAlphaView : View {
     }
 }
 
-struct WidgetAlphaProvider: TimelineProvider {
-    func placeholder(in context: Context) -> WidgetAlphaEntry {
-        return WidgetAlphaEntry(date: .now, value: "Hello")
+struct WidgetAlphaIntent: WidgetConfigurationIntent {
+    static let title: LocalizedStringResource = "Person"
+    
+    @Parameter(title: "Value")
+    var value: String?
+    
+    @Parameter(title: "First")
+    var first: WatcherEntity?
+}
+
+struct WatcherEntity: AppEntity, Identifiable, Hashable {
+    var id: String
+    var title: String
+    var value: String
+    
+    var displayRepresentation: DisplayRepresentation {
+        .init(title: "\(title): \(value)")
     }
     
-    func getSnapshot(in context: Context, completion: @escaping (WidgetAlphaEntry) -> Void) {
-        let entry = WidgetAlphaEntry(date: .now, value: "Hello")
-        completion(entry)
+    init(id: String, title: String, value: String) {
+        self.id = id
+        self.title = title
+        self.value = value
     }
     
-    func getTimeline(in context: Context, completion: @escaping (Timeline<WidgetAlphaEntry>) -> ()) {
-        Task {
-            let items = await getItems()
-            print("Widget \(items.count)")
-            //let value = "\(items.count)" // auth.demo() ?? "unknown"
-            var value = "n/a"
-            if let item = items.first,
-               let accessToken = await Auth.shared.refresh() {
-                let retrieved = await GoogleSheets.getValue(accessToken, item.spreadsheetId, item.sheetName, item.column, item.row)
-                value = retrieved == "" ? value : retrieved
-                print("widget value: \(value)")
+    init(watcher: Watcher) {
+        self.id = "\(watcher.id)"
+        self.title = watcher.title
+        self.value = watcher.value
+    }
+
+    static var typeDisplayRepresentation = TypeDisplayRepresentation(name: "Watcher")
+    static var defaultQuery = WatcherEntityQuery()
+}
+
+struct WatcherEntityQuery: EntityQuery, Sendable {
+    func entities(for identifiers: [WatcherEntity.ID]) async throws -> [WatcherEntity] {
+        return fetchProducts()
+            .filter { identifiers.contains("\($0.id)") }
+            .map(WatcherEntity.init)
+    }
+
+    func suggestedEntities() async throws -> [WatcherEntity] {
+        return fetchProducts().map(WatcherEntity.init)
+    }
+    
+    private static let container: ModelContainer = {
+            do {
+                return try ModelContainer(for: Watcher.self)
+            } catch {
+                fatalError("\(error)")
             }
-            
-            var entries: [WidgetAlphaEntry] = []
-            
-            let currentDate = Date()
-            for hourOffset in 0 ..< 5 {
-                let entryDate = Calendar.current.date(byAdding: .hour, value: hourOffset, to: currentDate)!
-                let entry = WidgetAlphaEntry(date: entryDate, value: "items")
-                entries.append(entry)
-            }
-            
-            let timeline = Timeline(entries: entries, policy: .atEnd)
-            completion(timeline)
-        }
-    }
+        }()
     
-    @MainActor
-    private func getItems() async -> [Watcher] {
-        do {
-            try? await Task.sleep(nanoseconds: 1_000_000)
-            let container = try ModelContainer(for: Watcher.self)
-            let descriptor = FetchDescriptor<Watcher>()
-            let items = try container.mainContext.fetch(descriptor)
-            return items
-        } catch {
-            print(error.localizedDescription)
-            return []
+    private func fetchProducts() -> [Watcher] {
+            do {
+                let context = ModelContext(Self.container)
+                let items = try context.fetch(FetchDescriptor<Watcher>())
+                return items
+            } catch {
+                print("Error fetching products: \(error)")
+                return []
+            }
         }
+}
+
+struct WidgetAlphaProvider: AppIntentTimelineProvider {
+    func placeholder(in context: Context) ->  WidgetAlphaEntry {
+        print("placeholder")
+        return WidgetAlphaEntry(date: .now, value: "plh")
+    }
+    func snapshot(for configuration: WidgetAlphaIntent, in context: Context) async -> WidgetAlphaEntry {
+        print("snapshot")
+        return WidgetAlphaEntry(date: .now, value: "snap")
+    }
+    func timeline(for configuration: WidgetAlphaIntent, in context: Context) async -> Timeline<WidgetAlphaEntry> {
+        let date = Calendar.current.date(byAdding: .hour, value: 1, to: .now)!
+        let value = configuration.value ?? "edit me"
+        let first = configuration.first?.title ?? "notitle"
+        let entry = WidgetAlphaEntry(date: date, value: first)
+        print("timeline")
+        return Timeline(entries: [entry], policy: .atEnd)
     }
 }
 
 struct WidgetAlpha: Widget {
+    let kind: String = "WidgetAlpha"
     var body: some WidgetConfiguration {
-        StaticConfiguration(kind: "WidgetAlpha", provider: WidgetAlphaProvider()) { entry in
+        AppIntentConfiguration(kind: "WidgetAlpha", intent: WidgetAlphaIntent.self, provider: WidgetAlphaProvider()) { entry in
             WidgetAlphaView(entry: entry)
-                .containerBackground(.fill.tertiary, for: .widget)
         }
         .configurationDisplayName("Alpha")
-        .supportedFamilies([.systemSmall])
+        .supportedFamilies([.systemSmall, .systemMedium])
     }
 }
 
--- Widgets/WidgetsBundle.swift
+++ Widgets/WidgetsBundle.swift
@@ -4,7 +4,7 @@ import SwiftUI
 @main
 struct WidgetsBundle: WidgetBundle {
     var body: some Widget {
-        Widgets()
+        //Widgets()
         WidgetAlpha()
     }
 }
